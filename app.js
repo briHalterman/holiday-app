7aad12840f7f3bf7d4a3e7dbad5f974ad8a19dac
Brittany Halterman <halterma@gmail.com>
2025-03-02 17:53:42 -0500

    Request, read, parse and print data

--- app.js
+++ app.js
@@ -2,9 +2,39 @@
 require('dotenv').config();
 
 const apiKey = process.env.API_KEY;
-
 // console.log(apiKey);
 
-// Request data
-// Parse data
-// Print data
+const date = '2025-12-25';
+const [year, month, day] = date.split('-');
+
+// Require https module
+const https = require('https');
+
+function getHolidays(countryCode) {
+  try {
+    // Request data
+    const request = https.get(
+      `https://holidays.abstractapi.com/v1/?api_key=${apiKey}&country=${countryCode}&year=${year}&month=${month}&day=${day}`,
+      (response) => {
+        let body = '';
+        // Read data
+        response.on('data', (data) => {
+          body += data.toString();
+        });
+
+        response.on('end', () => {
+          // Parse data
+          const holidays = JSON.parse(body);
+          // Print data
+          console.log(holidays);
+        });
+      }
+    );
+    request.on('error', (error) => console.error(error.message));
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+const query = process.argv.slice(2);
+query.forEach(getHolidays);
